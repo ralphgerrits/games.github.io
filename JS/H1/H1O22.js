945e65f20c13144b9a3f99627d110f1bb6e7616f
ralphgerrits <145121923+ralphgerrits@users.noreply.github.com>
2023-12-01 11:43:06 +0100

    Update H1O22.js

--- JS/H1/H1O22.js
+++ JS/H1/H1O22.js
@@ -2,7 +2,7 @@ var kleur = 'indianred';
 var afstand;
 
 function setup() {
-  canvas = createCanvas(450,450);
+  canvas = createCanvas(450, 450);
   canvas.parent('processing');
   textFont("Verdana");
   textSize(14);
@@ -12,41 +12,55 @@ function setup() {
 
 function draw() {
   background('lavender');
-  fill('black');  
-  
-  mouseX=constrain(mouseX,25,width - 25);
-  mouseY=constrain(mouseY,25,height - 25);
+  fill('black');
 
-  text("Beweeg de muis | x = " + round(mouseX) + " en y = " + round(mouseY),10,20);
-  
-  if (mouseX >= width - 30) {
-    kleur='red';
+  mouseX = constrain(mouseX, 25, width - 25);
+  mouseY = constrain(mouseY, 25, height - 25);
+
+  text("Beweeg de muis | x = " + round(mouseX) + " en y = " + round(mouseY), 10, 20);
+
+  // Kleur aanpassen aan de randen
+  if (mouseX >= width - 30 || mouseX <= 30 || mouseY <= 30 || mouseY >= height - 30) {
+    kleur = 'red';
+  } else {
+    kleur = 'indianred';
   }
-  else {
-    kleur='indianred';
+
+  // Afstand berekenen tot zwarte bol
+  afstand = dist(mouseX, mouseY, 225, 225);
+
+  // Kleur van JOS aanpassen op basis van afstand
+  if (afstand <= 5) {
+    kleur = 'red';
   }
-  
-  ellipse(width / 2,height / 2,100);
-  tekenJos(mouseX,mouseY,kleur);
-  afstand=dist(315,105,225,225);
-  text(afstand,10,40);
+
+  // Achtergrondkleur aanpassen op basis van positie van JOS
+  if (mouseX <= width / 2) {
+    background('yellow');
+  } else {
+    background('white');
+  }
+
+  ellipse(width / 2, height / 2, 100);
+  tekenJos(mouseX, mouseY, kleur);
+  text("Afstand tot zwarte bol: " + round(afstand), 10, 40);
 }
 
-function tekenJos(x,y,kleur) {
+function tekenJos(x, y, kleur) {
   push();
-  translate(x,y);
-  scale(1); 
+  translate(x, y);
+  scale(1);
   noStroke();
   fill(kleur);
-  ellipse(0,0,50);
+  ellipse(0, 0, 50);
   fill('slategray');
-  ellipse(-7,-10,17);
-  ellipse(7,-10,17);
+  ellipse(-7, -10, 17);
+  ellipse(7, -10, 17);
   fill('white');
-  ellipse(-7,-8,7,13);
-  ellipse(7,-8,7,13);
+  ellipse(-7, -8, 7, 13);
+  ellipse(7, -8, 7, 13);
   fill('orange');
-  ellipse(0,3,17);
+  ellipse(0, 3, 17);
   stroke('slategray');
   strokeWeight(3);
   fill('white');
